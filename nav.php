06808f93863d574678ebeb375c6a72bc0013d2c1
Brian C. Lindner <brian@bclindner.com>
2018-01-30 16:23:54 -0500

    1/30 classwork: nav consolidation & some basic templating

---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav>
+  <ul>
+      <li class="button navbutton">
+          <a href="topics.php">topics</a>
+      </li>
+      <li class="button navbutton">
+          <a href="editprofile.php">profile</a>
+      </li>
+  </ul>
+</nav>
