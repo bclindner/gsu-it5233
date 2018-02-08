21a60629eabb8184547dea0f9769fd98d0028b8c
Brian C. Lindner <brian@bclindner.com>
2018-02-08 15:15:55 -0500

    homework 2/8: sql changes

--- topics.sql
+++ topics.sql
@@ -1,23 +1,25 @@
 create table topics
 (
-  topicID integer not null unique,
+  topicID integer not null unique auto_increment,
   title varchar(64) not null,
   author integer not null,
   content varchar(2000) not null,
+  timeCreated datetime not null,
   primary key (topicID),
   foreign key (author) references users(userid)
 );
 create table comments
 (
-  commentID integer not null unique,
+  commentID integer not null unique auto_increment,
   author integer not null,
   content varchar(2000) not null,
+  timeCreated datetime not null,
   primary key (commentID),
   foreign key (author) references users(userid)
 );
 create table topic_comments
 (
-  topicID integer not null,
+  topicID integer not null auto_increment,
   commentID integer not null,
   constraint fk_topic_comment primary key (topicID, commentID)
 );
