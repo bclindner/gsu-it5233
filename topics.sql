df03b040d8146fd7289be0c8740786ac69d4ab4f
Brian C. Lindner <brian@bclindner.com>
2018-02-08 11:37:05 -0500

    homework 2/8: fixed and applied topics SQL file. may not be what thackston wants. oh well.

--- topics.sql
+++ topics.sql
@@ -1,21 +1,21 @@
-create table topic
+create table topics
 (
   topicID integer not null unique,
   title varchar(64) not null,
   author integer not null,
   content varchar(2000) not null,
   primary key (topicID),
-  foreign key (author) references user(userid)
+  foreign key (author) references users(userid)
 );
-create table comment
+create table comments
 (
   commentID integer not null unique,
   author integer not null,
   content varchar(2000) not null,
   primary key (commentID),
-  foreign key (author) references user(userid)
+  foreign key (author) references users(userid)
 );
-create table topic_comment
+create table topic_comments
 (
   topicID integer not null,
   commentID integer not null,
