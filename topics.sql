b2893e710a78b7705f8c319a39a53952f69dd844
Brian C. Lindner <brian@bclindner.com>
2018-02-13 11:18:24 -0500

    changed topics sql for simplicity's sake

--- topics.sql
+++ topics.sql
@@ -3,20 +3,18 @@ create table topics
 (
   topicID integer not null unique auto_increment,
   title varchar(64) not null,
-  author integer not null,
+  author varchar(255) not null, /* for simplicity - we'd normally use foreign key*/
   content varchar(2000) not null,
   timeCreated datetime not null,
   primary key (topicID),
-  foreign key (author) references users(userid)
 );
 create table comments
 (
   commentID integer not null unique auto_increment,
-  author integer not null,
+  author varchar(255) not null, /* for simplicity - we'd normally use foreign key*/
   content varchar(2000) not null,
   timeCreated datetime not null,
   primary key (commentID),
-  foreign key (author) references users(userid)
 );
 create table topic_comments
 (
