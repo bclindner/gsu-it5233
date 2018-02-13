37041fc4cd726ed03e142078d52654352b0d52e3
Brian C. Lindner <brian@bclindner.com>
2018-02-13 20:14:28 +0000

    fixed topic images

--- inc/topic.php
+++ inc/topic.php
@@ -5,7 +5,7 @@
   $content = $row['content'];
 ?>
   <div class="topic">
-      <img class="profimg" src="img/<?php echo $username; ?>" border="0" alt="<?php echo $username; ?>">
+      <img class="profimg" src="img/<?php echo $username; ?>.jpg" border="0" alt="<?php echo $username; ?>">
       <h2 class="topictitle"><?echo $title; ?></h2>
       <p class="byline">by <a href="profile.php"><?echo $username; ?></a> on <?php echo $date; ?></p>
       <p class="content"><?php echo $content; ?></p>
