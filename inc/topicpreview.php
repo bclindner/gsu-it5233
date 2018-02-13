f8d3056c52b84829c9138293ba2bc7f9777d990d
Brian C. Lindner <brian@bclindner.com>
2018-02-13 15:03:17 -0500

    topics now reference the proper comments page

--- inc/topicpreview.php
+++ inc/topicpreview.php
@@ -1,12 +1,13 @@
 <?php
 function topicPreview($row) {
+  $id = $row['id'];
   $author = $row['author'];
   $title = $row['title'];
   $datePosted = $row['timeCreated'];
 ?>
 <div class="topic">
   <img class="profimg" src="img/<?php echo $author ?>.jpg" border="0" alt="<?php echo $author ?>">
-    <a href="comments.php">
+    <a href="comments.php?topicid=<?php echo $id; ?>">
       <h2 class="topictitle"><?php echo $title; ?></h2>
     </a>
     <p class="byline">by <a href="profile.php"><?php echo $author ?></a> on <?php echo $datePosted ?></p>
