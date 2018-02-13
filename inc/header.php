d6ec701bad516609cf68fe89359d8ef907936c71
Brian C. Lindner <brian@bclindner.com>
2018-02-13 15:33:16 -0500

    header has logout now

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
             <h1>ribbit</h1>
         </a>
         <?php include "nav.php"; ?>
-        <div class="loggedin right">logged in as <strong>slippy</strong></div>
+        <div class="loggedin right">logged in as <strong>slippy</strong> | <strong><a href="logout.php">logout</a></div>
         <div class="clear"></div>
     </div>
 </header>
