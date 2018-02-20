0b1f8e349aee8013b73dd60e08c47d15e090f8f9
Brian C. Lindner <brian@bclindner.com>
2018-02-20 08:33:41 -0500

    implementation of reset.php PDO

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // query db for u/p combo using PDO
   $sql = "SELECT userid, is_admin FROM users WHERE username = ? and password = ?";
   $stm = $pdo->prepare($sql);
-  $res = $stm->execute([$username, $password]);
+  $stm->execute([$username, $password]);
   if($stm->rowCount() > 0){
     // get our user row
     $usr = $stm->fetch();
