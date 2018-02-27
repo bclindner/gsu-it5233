b1c84d294e199092f64a1858bfc77448407b71af
Brian C. Lindner <brian@bclindner.com>
2018-02-27 17:08:51 -0500

    comments page fix

--- inc/topic.php
+++ inc/topic.php
@@ -7,7 +7,7 @@
   <div class="topic">
       <img class="profimg" src="img/<?php echo $username; ?>.jpg" border="0" alt="<?php echo $username; ?>">
       <h2 class="topictitle"><?php echo $title; ?></h2>
-      <p class="byline">by <a href="profile.php"><?echo $username; ?></a> on <?php echo $date; ?></p>
+      <p class="byline">by <b><?php echo $username; ?></b> on <?php echo $date; ?></p>
       <p class="content"><?php echo $content; ?></p>
   </div>
 <?php } ?>
