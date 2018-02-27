c79ad530de52b7e14ba778c769a093af4cb24e7a
Brian C. Lindner <brian@bclindner.com>
2018-02-27 16:21:27 -0500

    whoops! forgot to set type here

--- auditlog.sql
+++ auditlog.sql
@@ -1,5 +1,5 @@
 CREATE TABLE auditlog (
-  logID not null auto_increment primary key,
+  logID integer not null auto_increment primary key,
   logDate datetime not null,
   logDesc varchar(255) not null,
   logIP varchar(45) not null,
