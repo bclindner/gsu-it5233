7bbf8d25cb6518a50e20ecba6c9f72ed149f5b41
Brian C. Lindner <brian@bclindner.com>
2018-03-01 16:07:18 -0500

    audit log error fix

--- inc/adminonly.php
+++ inc/adminonly.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
+require_once('log.php');
 $isadmin = $_SESSION['is_admin'];
 if($isadmin){
-  log("{$_SERVER['REQUEST_URI']}", $_SESSION['userid']);
+  auditLog("{$_SERVER['REQUEST_URI']}", $_SESSION['userid']);
   include('404.php');
   exit();
 }
--- inc/protected.php
+++ inc/protected.php
@@ -3,7 +3,7 @@ session_start();
 require_once('log.php');
 $userid = $_SESSION['userid'];
 if(empty( $userid )){
-  log("{$_SERVER['REQUEST_URI']}");
+  auditLog("{$_SERVER['REQUEST_URI']}");
   header('Location: index.php');
   exit();
 }
