3efb8d5bb0f0cc7d20152cb0a95fa896ca2958f9
Brian C. Lindner <brian@bclindner.com>
2018-03-05 16:15:59 -0500

    new approach for dbconn file

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,10 +1,16 @@
 <?php
-# access database credentials from a non-repo JSON file
-$creds = json_decode(file_get_contents("dbcreds.json"), true)[0];
-$pdo = NULL;
+# run this as a function so no global vars are overwritten or used
+# access database credentials from a non-repo INI file
+$creds = parse_ini_file('dbcreds.ini');
+$hostname = $creds['hostname'];
+$username = $creds['username'];
+$password = $creds['password'];
+$database = $creds['database'];
+# attempt the db connection (allow cached/persistent)
 try {
-  $pdo = new PDO("mysql:host={$creds['hostname']};dbname={$creds['database']}", $creds['username'], $creds['password'], array(PDO::ATTR_PERSISTENT => true));
+  $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 } catch (PDOException $e) {
+  # UNSAFE: catch and die() (this may leak server credentials and important info to an end user; change this later
   die("Connection failed: " . $e->getMessage());
 }
 ?>
