d3e664996cc4ba1cd1bdbf33fe6f3bfba692e8c6
Brian C. Lindner <brian@bclindner.com>
2018-03-05 16:47:09 -0500

    log file fix

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,16 +1,19 @@
 <?php
-# run this as a function so no global vars are overwritten or used
-# access database credentials from a non-repo INI file
-$creds = parse_ini_file('dbcreds.ini');
-$hostname = $creds['hostname'];
-$username = $creds['username'];
-$password = $creds['password'];
-$database = $creds['database'];
-# attempt the db connection (allow cached/persistent)
-try {
-  $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-} catch (PDOException $e) {
-  # UNSAFE: catch and die() (this may leak server credentials and important info to an end user; change this later
-  die("Connection failed: " . $e->getMessage());
+function dbconn(){
+  # access database credentials from a non-repo INI file
+  $creds = parse_ini_file('dbcreds.ini');
+  $hostname = $creds['hostname'];
+  $username = $creds['username'];
+  $password = $creds['password'];
+  $database = $creds['database'];
+  # attempt the db connection (allow cached/persistent)
+  try {
+    return new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+  } catch (PDOException $e) {
+    # UNSAFE: catch and die() (this may leak server credentials and important info to an end user; change this later
+    return NULL;
+    die("Connection failed: " . $e->getMessage());
+  }
 }
+$pdo = dbconn();
 ?>
--- inc/log.php
+++ inc/log.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once "inc/dbconn.php";
+require_once "dbconn.php";
 
 function auditLog($logDesc, $logUser=NULL){
+    global $pdo;
     $logIP = $_SERVER['REMOTE_ADDR'];
     $sql = "INSERT INTO auditLog (logDesc, logIP, logUser, logDate) VALUES (?, ?, ?, now())";
     $stm = $pdo->prepare($sql);
