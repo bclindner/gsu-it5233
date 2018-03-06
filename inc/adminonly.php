c8b0f6799bd19f21fe37af9d903dc66be35f3921
Brian C. Lindner <brian@bclindner.com>
2018-03-06 16:24:50 -0500

    adminonly access fix

--- inc/adminonly.php
+++ inc/adminonly.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'log.php';
 $isadmin = $_SESSION['is_admin'];
-if($isadmin){
+if(!$isadmin){
   auditLog("{$_SERVER['REQUEST_URI']}", $_SESSION['userid']);
   require '404.php';
   exit();
