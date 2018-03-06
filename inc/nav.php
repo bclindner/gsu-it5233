93b57d3797e864d7a8919e3f7df359bab49d99fc
Brian C. Lindner <brian@bclindner.com>
2018-03-06 16:11:29 -0500

    nav now has link to admin page, conditionally

--- inc/nav.php
+++ inc/nav.php
@@ -6,5 +6,10 @@
       <li class="button navbutton">
       <a href="editprofile.php?userid=<?php echo $_SESSION['userid']; ?>">profile</a>
       </li>
+      <?php if ($_SESSION['is_admin']){ ?>
+        <li class="button navbutton">
+            <a href="admin.php">admin</a>
+        </li>
+      <?php } ?>
   </ul>
 </nav>
