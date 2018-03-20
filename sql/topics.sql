6fccc1847c21da90d9407c45be3be68145d26080
Brian C. Lindner <brian@bclindner.com>
2018-03-20 16:03:18 -0400

    modified topics table to handle attachments (untested)

--- sql/topics.sql
+++ sql/topics.sql
@@ -1,12 +1,14 @@
 use IT5233;
 create table topics
 (
-  topicID varchar(16) not null unique,
+  topicID varchar(16) not null unique primary key,
   title varchar(64) not null,
   userID varchar(16) not null,
   content varchar(2000) not null,
   timeCreated datetime not null default current_timestamp,
-  primary key (topicID),
+  attachmentID integer null,
   foreign key (userID)
-  references users(userID)
+  references users(userID),
+  foreign key (attachmentID)
+  references attachments(attachmentID)
 );
