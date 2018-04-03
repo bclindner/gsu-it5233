5b9ca1248ec7a3dbaab83f07b0a1b2dcc7c93e43
Brian C. Lindner <brian@bclindner.com>
2018-04-03 15:01:13 -0400

    logout page session implementation

--- web/logout.php
+++ web/logout.php
@@ -1,9 +1,9 @@
 <?php
+require_once "inc/protected.php";
 require_once "inc/log.php";
-
+require_once "inc/session.php";
 session_start();
-auditLog("logout.php: Log out", $_SESSION['userid']);
-$_SESSION['userid'] = "";
+auditLog("logout.php: Log out", $session['userID']);
 header("Location: index.php");
 exit();
 ?>
