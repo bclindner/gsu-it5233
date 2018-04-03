a7d9c5ec1bbff8bf4dd99acbb32f84bb84a0ff14
Brian C. Lindner <brian@bclindner.com>
2018-04-03 15:37:56 -0400

    admin only implentation

--- web/inc/adminonly.php
+++ web/inc/adminonly.php
@@ -1,8 +1,18 @@
 <?php
-session_start();
 require_once 'log.php';
-$isadmin = $_SESSION['is_admin'];
-if(!$isadmin){
+require_once 'protected.php';
+$sql = "SELECT is_admin FROM users WHERE userID = ?";
+$stm = $pdo->prepare($sql);
+$res = $stm->execute($session['userID']);
+if ($res){
+  $row = $stm->fetch();
+  if ($row['is_admin'] != True){
+    auditLog("{$_SERVER['REQUEST_URI']}", $_SESSION['userid']);
+    require '404.php';
+    exit();
+  }
+}
+else {
   auditLog("{$_SERVER['REQUEST_URI']}", $_SESSION['userid']);
   require '404.php';
   exit();
