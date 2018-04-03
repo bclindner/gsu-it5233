b11ab709c45b99ee4c0bb663e0e0dde8c36dfb5b
Brian C. Lindner <brian@bclindner.com>
2018-04-03 11:58:37 -0400

    made log consistent with db structure

--- web/inc/log.php
+++ web/inc/log.php
@@ -5,7 +5,7 @@ require_once "dbconn.php";
 function auditLog($logDesc, $logUser=NULL){
     global $pdo;
     $logIP = $_SERVER['REMOTE_ADDR'];
-    $sql = "INSERT INTO auditLog (logDesc, logIP, logUser, logDate) VALUES (?, ?, ?, now())";
+    $sql = "INSERT INTO auditlog (logDesc, logIP, logUserID, logDate) VALUES (?, ?, ?, now())";
     $stm = $pdo->prepare($sql);
     $res = $stm->execute([$logDesc, $logIP, $logUser]);
 }
